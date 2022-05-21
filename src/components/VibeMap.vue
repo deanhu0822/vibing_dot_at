c769cad713ead2e9520ad1b7f28c17a68abe166e
Nathaniel Astudillo <nathanielastudillo@gmail.com>
2022-05-21 15:31:42 -0400

    added vuex

---
+++ src/components/VibeMap.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="vibe-map">
+        
+    </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    .vibe-map {
+        height: 100%;
+    }
+</style>
